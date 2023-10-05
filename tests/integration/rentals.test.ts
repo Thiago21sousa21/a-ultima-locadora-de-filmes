a3f64a11b3ae97a3d49750b05037c70637448bfa
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-10-05 14:27:54 -0300

    /:id

--- tests/integration/rentals.test.ts
+++ tests/integration/rentals.test.ts
@@ -1,5 +1,6 @@
 import supertest from 'supertest';
 import app from 'app';
+import { faker } from '@faker-js/faker';
 
 
 const server = supertest(app)
@@ -18,5 +19,25 @@ describe(" get /rentals", () => {
         })
       ])
     );
+  });
+})
+
+describe(" get /rentals/:id", () => {
+  it("", async () => {
+
+    const id = 'nan'
+
+    const { body, status } = await server.get(`/rentals/${id}`)
+    expect(status).toBe(400)
+    // expect(body).toEqual([] || expect.arrayContaining([
+    //   expect.objectContaining({
+    //       id: expect.any(Number),
+    //       date: expect.any(Date),
+    //       endDate: expect.any(Date),
+    //       userId: expect.any(Number),
+    //       closed: expect.any(Boolean),   
+    //     })
+    //   ])
+    // );
   });
 })
